47c1582195be3afbf1d78d9ee34b933586efe017
yukihiro ikeda <headlessrecord@gmail.com>
2022-12-09 08:01:50 +0900

    comments/index追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(params_valid)
+    if @comment.save
+      # ContactMailer.contact_mail(@blog).deliver
+      redirect_to comments_path, notice: "comment was successfully created." 
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+  def params_valid
+    params.require(:comment).permit(:content)
+  end
+end
