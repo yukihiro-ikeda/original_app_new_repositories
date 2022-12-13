1f12ed7f274746ff8d643b4b683543ae0368cc42
yukihiro ikeda <headlessrecord@gmail.com>
2022-12-13 20:44:52 +0900

    users/showを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teams#new'
+  resources :users, :only => [:show]
   # resources :comments, only: [:index]
   resources :teams
   resources :songs do
