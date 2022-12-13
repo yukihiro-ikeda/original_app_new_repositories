db139af8470e70f4776e8cff325ec1c5933ab4d6
yukihiro ikeda <headlessrecord@gmail.com>
2022-12-13 20:28:43 +0900

    routesにindexのみのcommentsのpathを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teams#new'
+  # resources :comments, only: [:index]
   resources :teams
   resources :songs do
     resources :comments
